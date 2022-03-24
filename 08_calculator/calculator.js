2fc1a336e7517d67bca14ceadaf29922fa07b283
diegofcn <94481027+diegofcn@users.noreply.github.com>
2022-03-24 19:08:05 +0100

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+     ? array.reduce((first, second) => first*second) : 0;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	return n === 0 ? 1 : n*factorial (n-1); 
 };
 
 // Do not edit below this line
